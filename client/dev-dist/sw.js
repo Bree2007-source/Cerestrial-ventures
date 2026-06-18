8618033000bcd015fad28612ee14a5554e02444b
Brendah Kathure <brendah.kathure@vault.lishailabs.com>
2026-06-18 12:22:23 +0300

    fix duplicate React with chart.js

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-9ba2d447'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.jetmsjghm8g"
+    "revision": "0.s3lvqo7rl2"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
