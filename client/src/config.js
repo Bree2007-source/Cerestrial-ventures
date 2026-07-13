c6b29f96c4e2f506212828e47c149b6f36d504cc
Brendah Kathure <brendah.kathure@vault.lishailabs.com>
2026-07-13 12:14:37 +0300

    Fix API_BASE_URL variable name in config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
+const API_BASE_URL = 'https://cerestrial-system.onrender.com/api'
 const API_HOST = API_BASE_URL.replace(/\/api$/, '');
 
 export { API_BASE_URL, API_HOST };
